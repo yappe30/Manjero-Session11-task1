9b101e1c23e6baca3ee216ad5383901c4144db66
El John <74305465+yappe30@users.noreply.github.com>
2023-03-08 00:27:07 +0800

    Add files via upload

---
+++ Session11-task1/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace costumer.Model
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+    }
+}
